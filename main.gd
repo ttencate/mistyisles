da7ddf325d4b2b792216d394683b0f0fec74dec8
Thomas ten Cate <ttencate@gmail.com>
2018-02-25 12:13:59 +0100

    Add more levels

--- main.gd
+++ main.gd
@@ -1,30 +1,32 @@
 extends Node
 
-var LEVELS = [
-	preload("res://levels/level_01.tscn"),
-	preload("res://levels/level_02.tscn"),
-]
-var current_level = 0
+var current_level = 1
 var level_node
 
 func _ready():
 	create_level(current_level)
 
-func create_level(name):
+func create_level(level_number):
+	var level_scene = load("res://levels/level_%02d.tscn" % level_number)
+	if not level_scene:
+		print("You win!") # TODO
+		return
 	level_node = preload("res://level.tscn").instance()
-	level_node.create(LEVELS[current_level].instance())
+	level_node.create(level_scene.instance())
 	get_node("level_root").add_child(level_node)
 	level_node.connect("solved", self, "level_solved")
 
+func switch_level(level_number):
+	if level_node:
+		get_node("level_root").remove_child(level_node)
+		level_node.queue_free()
+		level_node = null
+	
+	current_level = level_number
+	create_level(current_level)
+
 func level_solved():
-	get_node("level_root").remove_child(level_node)
-	level_node.queue_free()
-	current_level += 1
-	if current_level < len(LEVELS):
-		create_level(current_level)
-	else:
-		print("You win!") # TODO
-		pass
+	switch_level(current_level + 1)
 
 func _input(event):
 	if OS.has_feature("debug"):
@@ -32,3 +34,6 @@ func _input(event):
 			match event.scancode:
 				KEY_ESCAPE:
 					get_tree().quit()
+					return
+			if event.unicode >= 97 and event.unicode <= 122:
+				switch_level(event.unicode - 97 + 1)
